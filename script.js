a13f551ebb2f3b96fc4a9986438176ea52df175e
Nick Varikash <n.varicash@gmail.com>
2022-09-08 20:00:33 +0300

    Добавил изменение имени и title через модальное окно

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 let profileButton = document.querySelector('.profile__title-setting');
 
 
+let profileName = document.querySelector('.profile__title');
+let profileTitle = document.querySelector('.profile__subtitle');
+
 let popup = document.querySelector('.popup');
 let popupCloseButton = popup.querySelector('.popup__close-button');
 let popupSaveButton = popup.querySelector('.popup__button');
+let popupInputName = popup.querySelector('.popup__input[name="profile-name"]');
+let popupInputTitle = popup.querySelector('.popup__input[name="profile-title"]');
 
 profileButton.addEventListener('click', function () {
   popup.classList.add('popup_opened');
@@ -14,5 +19,12 @@ popupCloseButton.addEventListener('click', function () {
 })
 
 popupSaveButton.addEventListener('click', function () {
-  alert('событие');
+  if (popupInputName.value.length > 0) {
+    profileName.textContent = popupInputName.value;
+  }
+
+  if (popupInputTitle.value.length > 0) {
+    profileTitle.textContent = popupInputTitle.value;
+  }
 })
+
