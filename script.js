d6adab81d0342cf644123e6ff05642616bf06372
Nick Varikash <n.varicash@gmail.com>
2022-09-08 13:29:48 +0300

    Реализовал частичный функционал модального окна

--- script.js
+++ script.js
@@ -2,4 +2,17 @@ let profileButton = document.querySelector('.profile__title-setting');
 
 
 let popup = document.querySelector('.popup');
+let popupCloseButton = popup.querySelector('.popup__close-button');
+let popupSaveButton = popup.querySelector('.popup__button');
 
+profileButton.addEventListener('click', function () {
+  popup.classList.add('popup_opened');
+})
+
+popupCloseButton.addEventListener('click', function () {
+  popup.classList.remove('popup_opened');
+})
+
+popupSaveButton.addEventListener('click', function () {
+  alert('событие');
+})
