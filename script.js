44c0eb5d9c04bb137bc840348f083ad4498ad672
Nick Varikash <n.varicash@gmail.com>
2022-09-09 09:33:19 +0300

    комментарии к коду

--- script.js
+++ script.js
@@ -1,22 +1,29 @@
-let profileButton = document.querySelector('.profile__title-setting');
+//Открытие-закрытие модального окна + редактирование имени
 
+let profileButton = document.querySelector('.profile__title-setting'); //кнопка редактирования профиля (открытие)
 
-let profileName = document.querySelector('.profile__title');
-let profileTitle = document.querySelector('.profile__subtitle');
+let profileName = document.querySelector('.profile__title'); //Имя профиля на странице
+let profileTitle = document.querySelector('.profile__subtitle'); //Титул профиля на странице
 
-let popup = document.querySelector('.popup');
-let popupCloseButton = popup.querySelector('.popup__close-button');
-let popupSaveButton = popup.querySelector('.popup__button');
-let popupInputName = popup.querySelector('.popup__input[name="profile-name"]');
-let popupInputTitle = popup.querySelector('.popup__input[name="profile-title"]');
+let popup = document.querySelector('.popup'); //модальное окно
+let popupCloseButton = popup.querySelector('.popup__close-button'); //кнопка закрытия модального окна
+let popupSaveButton = popup.querySelector('.popup__button'); //кнопка сохранения
+let popupInputName = popup.querySelector('.popup__input[name="profile-name"]'); //Инпут имени в модальном окне
+let popupInputTitle = popup.querySelector('.popup__input[name="profile-title"]'); //Импут титула в модальном окне
+
+//функция открытия модального окна
 
 profileButton.addEventListener('click', function () {
   popup.classList.add('popup_opened');
 })
 
+//функция закрытия модального окна
+
 popupCloseButton.addEventListener('click', function () {
   popup.classList.remove('popup_opened');
 })
+
+// функция присвоения значений инпутов имени и титулу профиля на сайте.
 
 popupSaveButton.addEventListener('click', function () {
   if (popupInputName.value.length > 0) {
@@ -27,4 +34,4 @@ popupSaveButton.addEventListener('click', function () {
     profileTitle.textContent = popupInputTitle.value;
   }
 })
-
+//----------------------------------------------------------------------------------------
