5100309152e5ee63164a36915d44c80867a96904
Nick Varikash <n.varicash@gmail.com>
2022-09-27 20:15:55 +0300

    рефакторинг

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 //Открытие-закрытие модального окна + редактирование имени
 
 let profileButton = document.querySelector('.profile__title-setting'); //кнопка редактирования профиля (открытие)
@@ -136,7 +135,7 @@ function addNewCard(e) {
 }
 
 /**
- * Добавление дефолтных карточек
+ * Функция добавления дефолтных карточек + удаление и лайки
  */
 function initialCardsAdd(placeName, placeLink) {
   const cardTemplate = document.querySelector('#place-card').content;
@@ -156,167 +155,6 @@ function initialCardsAdd(placeName, placeLink) {
   places.prepend(placeCard);
 }
 
-for (let i = 0; i < initialCards.length; i++) {
-  initialCardsAdd(initialCards[i].name, initialCards[i].link);
-}
-=======
-//Открытие-закрытие модального окна + редактирование имени
-
-let profileButton = document.querySelector('.profile__title-setting'); //кнопка редактирования профиля (открытие)
-
-let profileName = document.querySelector('.profile__title'); //Имя профиля на странице
-let profileTitle = document.querySelector('.profile__subtitle'); //Титул профиля на странице
-
-let popup = document.querySelector('.popup'); //модальное окно
-let popupCloseButton = popup.querySelector('.popup__close-button'); //кнопка закрытия модального окна
-let popupSaveButton = popup.querySelector('.popup__button'); //кнопка сохранения
-let popupInputName = popup.querySelector('.popup__input[name="profile-name"]'); //Инпут имени в модальном окне
-let popupInputTitle = popup.querySelector('.popup__input[name="profile-title"]'); //Импут титула в модальном окне
-const places = document.querySelector('.places__cards-grid');
-
-//функция открытия модального окна
-
-profileButton.addEventListener('click', function () {
-  popup.classList.add('popup_opened');
+initialCards.forEach(function(initialCard) {
+  initialCardsAdd(initialCard.name, initialCard.link);
 })
-
-//функция закрытия модального окна
-
-function closePopup() {
-  popup.classList.remove('popup_opened');
-}
-
-popupCloseButton.addEventListener('click', closePopup);
-
-// функция присвоения значений инпутов имени и титулу профиля на сайте.
-
-popup.addEventListener('submit', function (e) {
-  e.preventDefault();
-
-  if (popupInputName.value.length > 0) {
-    profileName.textContent = popupInputName.value;
-  }
-
-  if (popupInputTitle.value.length > 0) {
-    profileTitle.textContent = popupInputTitle.value;
-  }
-
-  popupInputName.value = profileName.textContent;
-  popupInputTitle.value = profileTitle.textContent;
-
-  closePopup();
-
-})
-//----------------------------------------------------------------------------------------
-
-//Открытие-закрытие модального окна для добавления нового места
-
-const popupPlaces = document.querySelector('#new-place'); //модальное окно для добавления места
-const placeButton = document.querySelector('.profile__add-button'); //кнопка открытия модального окна
-const popupCloseButtonSecond = popupPlaces.querySelector('#closeBtn-2'); //кнопка закрытия модального окна
-const popupPlaceName = popupPlaces.querySelector('.popup__input[name="place-name"]');
-const popupPlaceLink = popupPlaces.querySelector('.popup__input[name = "place-link"]');
-
-/* Функция открытия модального окна */
-
-placeButton.addEventListener('click', function () {
-  popupPlaces.classList.add('popup_opened');
-})
-
-/* Функция закрытия модального окна */
-
-popupCloseButtonSecond.addEventListener('click', function () {
-  popupPlaces.classList.remove('popup_opened');
-  popupPlaceName.value = '';
-  popupPlaceLink.value = '';
-})
-
-/*---------------------------------------------------------------------------------------*/
-
-
-/**
- * Дефолтный набор карточек
- */
-const initialCards = [
-  {
-    name: 'Элиста, Калмыкия',
-    link: './images/kalmikia.jpeg'
-  },
-  {
-    name: 'Алтай',
-    link: './images/altai.jpeg'
-  },
-  {
-    name: 'Чиркейская ГЭС',
-    link: './images/dagestan.jpeg'
-  },
-  {
-    name: 'Домбай',
-    link: './images/dombai.jpg'
-  },
-  {
-    name: 'Эльбрус',
-    link: './images/elbrus.jpg'
-  },
-  {
-    name: 'Карачаевск',
-    link: './images/karachaesk.jpeg'
-  }
-];
-
-
-
-
-/**
- * добавляем новую карточку
- */
-popupPlaces.addEventListener('submit', addNewCard);
-
-
-
-/****************************ФУНКЦИИ*************************************/
-
-/**
- * @param {Event} e
- * функция добавления новой карточки
- */
-function addNewCard(e) {
-  e.preventDefault();
-
-  if (popupPlaceName.value.length > 0 && popupPlaceLink.value.length > 0) {
-    initialCardsAdd(popupPlaceName.value, popupPlaceLink.value);
-  } else {
-    alert('Для добавления места необходимо заполнить все поля.')
-    addNewCard();
-  }
-
-  popupPlaceName.value = '';
-  popupPlaceLink.value = '';
-  popupPlaces.classList.remove('popup_opened');
-}
-
-/**
- * Добавление дефолтных карточек
- */
-function initialCardsAdd(placeName, placeLink) {
-  const cardTemplate = document.querySelector('#place-card').content;
-  const placeCard = cardTemplate.querySelector('.place').cloneNode(true);
-
-  placeCard.querySelector('.place__image').src = placeLink;
-  placeCard.querySelector('.place__title').textContent = placeName;
-
-  placeCard.querySelector('.place__button').addEventListener('click', function (e) {
-    e.target.classList.toggle('place__button_active');
-  })
-
-  placeCard.querySelector('.place__delete').addEventListener('click', function (e) {
-    e.target.closest('.place').remove();
-  })
-
-  places.prepend(placeCard);
-}
-
-for (let i = 0; i < initialCards.length; i++) {
-  initialCardsAdd(initialCards[i].name, initialCards[i].link);
-}
->>>>>>> feature/imagePopup
